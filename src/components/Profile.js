933d081dbf30f2f94ed183aa49edad8c706f3c34
Darsshan <darsshanpalaram@gmail.com>
2022-12-08 09:21:10 +0000

    Adds the beginning of a profile page

---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styling/Profile.css";
+
+export default function Profile() {
+  return (
+    <div>
+      <h2 id="log-in-message">Please log in</h2>
+    </div>
+  );
+}
